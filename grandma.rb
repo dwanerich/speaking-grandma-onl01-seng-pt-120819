c6b80cd5268212eb6be37a49809d90fc451dfb71
dwane richards <dwanerich@gmail.com>
2019-12-11 03:38:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@ def speak_to_grandma(phrase)
       return "I LOVE YOU TOO PUMPKIN!"  
   elsif
     phrase == phrase.upcase
-      return "NO, NOT SINCE 1938"
+      return "NO, NOT SINCE 1938!"
   end
 
 end
